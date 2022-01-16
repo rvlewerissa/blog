f4f62010bc3d060f1fa099496617c39cb8a50980
Vitto Lewerissa <rvlewerissa@gmail.com>
2022-01-17 06:30:20 +0700

    style: minor font style fix for filename code highlighter

--- apps/blog/src/components/CodeHighlighter.tsx
+++ apps/blog/src/components/CodeHighlighter.tsx
@@ -16,9 +16,12 @@ type PreProps = {
   children: React.ReactNode;
 };
 
+const CODE_ADDED_COLOR = '#043E2F';
+const CODE_REMOVED_COLOR = '#831843';
+
 const Code = (props: Props) => {
   const match = /language-(\w+)/.exec(props.className || '');
-  console.log(props);
+
   return (
     <SyntaxHighlighter
       language={Array.isArray(match) ? match[1] : ''}
@@ -29,7 +32,9 @@ const Code = (props: Props) => {
             {props.filename && (
               <div className='bg-zinc-800 mb-5 flex rounded-t-lg'>
                 <div className='pt-3 px-4 pb-1 text-base font-medium text-gray-300 border-b border-zinc-400'>
-                  <span className='font-sans'>{props.filename}</span>
+                  <span className='font-sans text-sm lg:text-base'>
+                    {props.filename}
+                  </span>
                 </div>
                 <div className='flex-1 h-8 self-end bg-zinc-700 rounded-tl-sm border border-r-0 border-zinc-600' />
               </div>
@@ -48,12 +53,12 @@ const Code = (props: Props) => {
       lineProps={(lineNumber: number) => {
         let backgroundColor;
         if (props.added && shouldHighlight(lineNumber, props.added)) {
-          backgroundColor = '#043E2F';
+          backgroundColor = CODE_ADDED_COLOR;
         } else if (
           props.removed &&
           shouldHighlight(lineNumber, props.removed)
         ) {
-          backgroundColor = '#831843';
+          backgroundColor = CODE_REMOVED_COLOR;
         }
         return {
           style: {
